4bf02461ae9c698b424e060946b15f2e03886a16
atinfinity <dandelion1124@gmail.com>
2019-03-11 01:42:52 +0900

    changed to make session in init()

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,12 +1,23 @@
 from styx_msgs.msg import TrafficLight
 import numpy as np
 import tensorflow as tf
+import time
 import rospy
 
 class TLClassifier(object):
     def __init__(self):
         self.graph = self.load_graph('light_classification/frozen_model/frozen_inference_graph.pb')
         self.threshold = .5
+
+        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+        self.detect_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+        self.detect_scores = self.graph.get_tensor_by_name('detection_scores:0')
+        self.detect_classes = self.graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.graph.get_tensor_by_name('num_detections:0')
+
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(graph=self.graph, config=config)
 
     def load_graph(self, graph_file):
         """Loads a frozen inference graph"""
@@ -33,28 +44,26 @@ class TLClassifier(object):
         #TODO implement light color prediction
         state = TrafficLight.UNKNOWN
 
+
+        image_expanded = np.expand_dims(image, axis=0)
         with self.graph.as_default():
-            with tf.Session(graph=self.graph) as sess:
-                image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
-                detect_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
-                detect_scores = self.graph.get_tensor_by_name('detection_scores:0')
-                detect_classes = self.graph.get_tensor_by_name('detection_classes:0')
-                num_detections = self.graph.get_tensor_by_name('num_detections:0')
 
-                image_expanded = np.expand_dims(image, axis=0)
-                (boxes, scores, classes, num) = sess.run(
-                    [detect_boxes, detect_scores, detect_classes, num_detections],
-                    feed_dict={image_tensor: image_expanded})
+            #start = time.time()
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.detect_boxes, self.detect_scores, self.detect_classes, self.num_detections],
+                feed_dict={self.image_tensor: image_expanded})
+            #end = time.time()
+            #rospy.loginfo("inference time: %s", str((end - start) * 1000))
 
-                classes = np.squeeze(classes).astype(np.int32)
-                scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+        scores = np.squeeze(scores)
 
-                if scores[0] > self.threshold:
-                    if classes[0] == 1:
-                        state = TrafficLight.GREEN
-                    elif classes[0] == 2:
-                        state = TrafficLight.RED
-                    elif classes[0] == 3:
-                        state = TrafficLight.YELLOW
+        if scores[0] > self.threshold:
+            if classes[0] == 1:
+                state = TrafficLight.GREEN
+            elif classes[0] == 2:
+                state = TrafficLight.RED
+            elif classes[0] == 3:
+                state = TrafficLight.YELLOW
 
         return state
